7e04c9025cdec2eac72ca7299e3be371355d17a9
Cole Hafner <cole.hafner@soliddigital.com>
2017-10-17 13:02:07 -0700

    fixed errors with tape unit tests.

--- test/Gallery.spec.js
+++ test/Gallery.spec.js
@@ -1,14 +1,14 @@
 import test from 'tape'
 import { shallow } from 'vue-test-utils'
-import List from '../src/components/gallery/page.vue'
+import Gallery from '../src/components/gallery/page.vue'
 
 test('Gallery.vue', t => {
-	t.plan(3); // 1 test only
-  const wrapper = shallow(List);
+	t.plan(3); // 3 test only
+  const wrapper = shallow(Gallery);
   const msgOne = 'should have a root element with a "container" class';
   const msgTwo = 'should have 18 cells (1 for each photo)';
   const msgThree = 'should have 5 rows of photos';
-  t.equal(wrapper.findAll('.container').length, 1, messageOne);
+  t.equal(wrapper.findAll('.container').length, 1, msgOne);
   t.equal(wrapper.findAll('.col').length, 18, msgTwo);
   t.equal(wrapper.findAll('.row').length, 5, msgThree);
 });
