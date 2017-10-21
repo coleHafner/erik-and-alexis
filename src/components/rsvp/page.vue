ce6c5af583016fb45cc33356151b5382211ef7f2
Cole Hafner (Personal MacBook) <colehafner@gmail.com>
2017-10-21 15:02:27 -0700

    Added 'attending' field to rsvp form. Changed up error/success styles. Hooked up to backend.

--- src/components/rsvp/page.vue
+++ src/components/rsvp/page.vue
@@ -6,8 +6,8 @@
 				| &nbsp;
 
 			.col.s8
-				.error.center-align(v-show="error")
-					| {{ error }}
+				.message.center-align(v-show="message" v-bind:class="messageClass")
+					| {{ message }}
 			.col.s2
 				| &nbsp;
 		.row
@@ -22,13 +22,18 @@
 							v-text-input#party-name(name="partyName" v-model="form.partyName")
 							label(for="party-name") Party Name
 
-				.row
+				.row.attending
+					.col.s12
+						v-icon.left(prefix="") date_range
+						v-switch.left(v-model="form.amAttending" on="Yes!" off="No")
+
+				.row(v-show="form.amAttending")
 					.col.s12
 						.input-field
 							v-icon(prefix="") face
 							v-text-input#total-adults(name="totalAdults" v-model="form.totalAdults")
 							label(for="total-adults") Total Adults
-				.row
+				.row(v-show="form.amAttending")
 					.col.s12
 						.input-field
 							v-icon(prefix="") child_care
@@ -48,31 +53,56 @@ export default {
 	name: 'vue-rsvp-page',
 	data() {
 		return {
-			error: '',
+			message: '',
+			success: false,
 			form: {
 				partyName: '',
 				totalAdults: '',
 				totalChildren: '',
+				amAttending: false,
 			},
 		};
 	},
+	computed: {
+		messageClass() {
+			return this.success === false ? 'error' : 'success';
+		},
+	},
 	methods: {
 		submit() {
-			this.error = '';
+			this.message = '';
+			this.success = false;
 
 			if (
-				!this.form.partyName ||
-				!this.form.totalAdults
+				!this.form.partyName
 			) {
-				console.log('here');
-				this.error = 'You must fill in all fields';
+				this.message = 'You must fill in all fields';
 				return false;
 			}
 
+			if (this.form.amAttending && !this.form.totalAdults) {
+				this.message = 'Please specify the number of adults in your party';
+				return false;
+			}
+
+			// set default values
+			this.form.totalAdults = this.form.totalAdults || 0;
+			this.form.totalChildren = this.form.totalChildren || 0;
+
 			// submit...
-			this.$http.get('http://google.com')
-				.then((reply) => {
-					console.log('this is reply...', reply);
+			this.$http.post('http://rsvp-me.dev/api/rsvps', this.form)
+				.then(() => {
+					// reset form
+					this.form.totalChildren = '';
+					this.form.totalAdults = '';
+					this.form.partyName = '';
+					this.form.amAttending = false;
+
+					this.success = true;
+					this.message = 'RSVP Successful!';
+				})
+				.catch((err) => {
+					this.message = err.bodyText || err.body;
 				});
 			return '';
 		},
@@ -86,17 +116,39 @@ export default {
 	padding-bottom: 40px;
 	border: 1px solid #ccc;
 	border-radius: 5px;
+
+	.attending {
+		padding-bottom: 15px;
+
+		.material-icons {
+			font-size: 2rem;
+		}
+
+		.switch.left {
+			label {
+				font-size: 14px !important;
+			}
+		}
+	}
 }
 
 .container {
 	margin-top: 50px;
 }
 
-.error {
-	background-color: red;
+.message {
 	border-radius: 5px;
 	font-weight: bold;
 	padding: 10px;
-	color: #fff;
+	color: rgba(0, 0, 0, .87);
+	border: 1px solid #000;
+}
+
+.error {
+	border-color: red;
+}
+
+.success {
+	border-color: green;
 }
 </style>
